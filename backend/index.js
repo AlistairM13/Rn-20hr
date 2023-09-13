1cd265beb75a5f90bfe8b2cb23c675e17e1b3e1b
Alistair <alistairm2022@gmail.com>
2023-09-13 11:04:07 +0530

    user route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,44 @@
 const express = require('express')
-
+const skillRouter = require('./routes/skills-router')
+const userRouter = require('./routes/users-router')
+const mongoose = require('mongoose')
 const app = express()
+const HttpError = require('./models/http-error')
 
-app.listen(5000, ()=>{
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+});
+
+app.use('/api/skills', skillRouter)
+app.use('/api/users', userRouter)
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://Alistair:l70uc1YBSA9sZchW@cluster0.ajs1ajv.mongodb.net/The20HrRule?retryWrites=true&w=majority"
+  )
+  .then(() => {
     console.log("server started")
-})
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ backend/models/user-model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    following: [{ type: mongoose.Types.ObjectId, required: true, default: [], ref: 'User' }],
+    followers: [{ type: mongoose.Types.ObjectId, required: true, default: [], ref: 'User' }],
+    skills: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Skill' }]
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
